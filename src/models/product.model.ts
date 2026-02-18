ef9e02e7a618872283fc3d65831ae1ed450ffc64
luisinavinuela <luisina.v@hotmail.com>
2026-02-18 18:11:40 -0300

    Auth y Filtros funcionando

---
+++ src/models/product.model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, default: 0 },
+  stock: { type: Number, default: 0 },
+  description: { type: String, default: "Sin descripción" },
+  category: { type: String, default: "Sin categoria" }
+}, {
+  versionKey: false
+})
+
+const Product = mongoose.model("Product", ProductSchema)
+
+export { Product }
